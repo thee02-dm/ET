0f0761da16c874544fed4a28bd743fee9bd131ca
thee02-dm <theerajh17@gmail.com>
2025-03-11 23:38:17 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ st.title('Machine Learning App')
 
 st.info ('This App is built on Machine Learning Model')
 
-df = pd.read_csv ('https://raw.githubusercontent.com/thee02-dm/yt-ml/refs/heads/master/.streamlit/penguins_cleaned.csv'
+df = pd.read_csv ('https://raw.githubusercontent.com/thee02-dm/yt-ml/refs/heads/master/.streamlit/penguins_cleaned.csv')
 df
