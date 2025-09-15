73ec7376ec0d68e94bc86f48447e7fe8430df0ff
thee02-dm <theerajh17@gmail.com>
2025-09-15 11:59:38 +0800

    Delete streamlit_app.py

--- streamlit_app.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import pandas 
-st.title('Machine Learning App')
-
-st.info ('This App is built on Machine Learning Model')
-
-df = pd.read_csv ('https://raw.githubusercontent.com/thee02-dm/yt-ml/refs/heads/master/.streamlit/penguins_cleaned.csv')
-df
